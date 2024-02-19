42cab9cbf9397ebfa667116e0c859e18c1f0b1bf
Nyasha D. Chiroro <chirorodennis@gmail.com>
2024-02-19 16:16:12 +0200

    fix padding issue

--- src/app/components/nav-bar.component.ts
+++ src/app/components/nav-bar.component.ts
@@ -16,7 +16,7 @@ import { ColorModeSwitchComponent } from './color-mode-switch/color-mode-switch.
             display: flex;
             justify-content: space-between;
             align-items: center;
-            padding-inline: 10px;
+            padding: 10px;
         }
 
         nav img {
