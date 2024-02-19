dafc03ce358d9f882dab9b3f02d5ab454e2e82cd
Nyasha D. Chiroro <chirorodennis@gmail.com>
2024-02-19 08:27:23 +0200

    create a responsive layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
+import { initFlowbite } from 'flowbite'
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'game-hub-ng';
+export class AppComponent implements OnInit {
+    title = 'game-hub-ng'
+
+    ngOnInit() {
+        initFlowbite()
+    }
 }
