26ea55fba64a1efc97fea7d5e3109830b10a2bf4
Nyasha D. Chiroro <chirorodennis@gmail.com>
2024-02-21 16:37:34 +0200

    highlight selected genre

--- src/app/components/genre-list/genre-list.component.ts
+++ src/app/components/genre-list/genre-list.component.ts
@@ -1,5 +1,12 @@
 import { AsyncPipe, CommonModule, NgFor } from '@angular/common'
-import { Component, EventEmitter, OnInit, Output, inject } from '@angular/core'
+import {
+    Component,
+    EventEmitter,
+    Input,
+    OnInit,
+    Output,
+    inject,
+} from '@angular/core'
 import { Genre, GenresService } from '../../services/genres.service'
 import { EMPTY, Observable, catchError } from 'rxjs'
 import { CropImagePipe } from '../../pipes/crop-image.pipe'
@@ -16,7 +23,17 @@ export class GenreListComponent implements OnInit {
     genresService = inject(GenresService)
     errorMessage = ''
 
+    @Input() selectedGenre!: Genre | null
+
     @Output() selectGenre = new EventEmitter()
+
+    selectedGenreClasses(id: number) {
+        return {
+            'font-bold': this.selectedGenre
+                ? this.selectedGenre.id === id
+                : false,
+        }
+    }
 
     onSelectGenre(genre: Genre) {
         this.selectGenre.emit(genre)
